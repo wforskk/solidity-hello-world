08aad56e7c798bcaf6a925ea8834dddf67c7c00c
hulkkk <wfoskk@gmail.com>
2021-11-29 15:31:32 +0900

    Initialize repository

---
+++ contracts/Greeter.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+import "openzeppelin-solidity/contracts/access/Ownable.sol";
+
+contract Greeter is Ownable {
+    string private _greeting = "Hello, World!";
+
+    function greet() external view returns (string memory) {
+        return _greeting;
+    }
+
+    function setGreeting(string calldata greeting) external onlyOwner {
+        _greeting = greeting;
+    }
+}
+
+// contract Greeter {
+//     string private _greeting = "Hello, World!";
+//     address private _owner;
+
+//     constructor() {
+//         _owner = msg.sender;
+//     }
+
+//     modifier onlyOwner() {
+//         require(
+//             msg.sender == _owner,
+//             "Ownable: caller is not the owner"
+//         );
+//         _;
+//     }
+
+//     function greet() external view returns (string memory) {
+//         return _greeting;
+//     }
+//     function setGreeting(string calldata greeting) external onlyOwner{
+//         _greeting = greeting;
+//     }
+//     function owner() public view returns(address) {
+//         return _owner;
+//     }
+// }
